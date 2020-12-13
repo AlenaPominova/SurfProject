4614ff1cb7bad0c646f142ecdcb210c9ed66797e
Alena Pominova <alena.pominova@gmail.com>
2020-12-13 15:25:02 +0300

    Add fragments style

--- app/src/main/java/com/pominova/surfmemesapp/fragment/ProfileFragment.kt
+++ app/src/main/java/com/pominova/surfmemesapp/fragment/ProfileFragment.kt
@@ -1,6 +1,28 @@
 package com.pominova.surfmemesapp.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.pominova.surfmemesapp.R
+import com.zharkovsky.memes.viewModels.ProfileViewModel
 
 class ProfileFragment : Fragment() {
+    private lateinit var profileViewModel: ProfileViewModel
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        profileViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+        val root = inflater.inflate(R.layout.profile_fragment, container, false)
+        val textView: TextView = root.findViewById(R.id.profile_tv)
+        profileViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
 }
