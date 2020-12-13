110c6bbbc7e0661efdd7e7e5d0532ce0e46c7ae3
Alena Pominova <alena.pominova@gmail.com>
2020-12-13 14:50:12 +0300

    Init main screen

--- app/src/main/java/com/pominova/surfmemesapp/activity/MainActivity.kt
+++ app/src/main/java/com/pominova/surfmemesapp/activity/MainActivity.kt
@@ -2,6 +2,9 @@ package com.pominova.surfmemesapp.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.pominova.surfmemesapp.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        val navigationView: BottomNavigationView = findViewById(R.id.navigation_view)
+        navigationView.itemIconTintList = null
+
+        val navController = findNavController(R.id.navigation_host_fragment)
+        navigationView.setupWithNavController(navController)
     }
 }
