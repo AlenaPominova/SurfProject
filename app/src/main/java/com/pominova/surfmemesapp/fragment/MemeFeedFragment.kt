8140b20abc13dc777b08263599a23b291f4cc140
Alena Pominova <alena.pominova@gmail.com>
2020-12-13 15:44:36 +0300

    Add meme cell

--- app/src/main/java/com/pominova/surfmemesapp/fragment/MemeFeedFragment.kt
+++ app/src/main/java/com/pominova/surfmemesapp/fragment/MemeFeedFragment.kt
@@ -6,20 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.pominova.surfmemesapp.R
 import com.zharkovsky.memes.viewModels.DashboardViewModel
 
 class MemeFeedFragment : Fragment() {
     private lateinit var dashboardViewModel: DashboardViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+
         val root = inflater.inflate(R.layout.meme_feed_fragment, container, false)
+        val recyclerView = root.findViewById<RecyclerView>(R.id.recycler_list)
+        recyclerView.setHasFixedSize(true)
+        val layoutManager =
+            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        recyclerView.layoutManager = layoutManager
 
         return root
     }
